d29e307921c0a46c6f079c21901f066a65aee8a6
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-10-10 10:57:16 +0500

    CORS ORIGIN [ADD]

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
 
 mongoose
   .connect(process.env.MONGO_URI)
