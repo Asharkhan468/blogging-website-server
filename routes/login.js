acd287a7c3547efddf854193b7ed79dd45c260bb
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-10-21 17:57:48 +0500

    PATH ADD

--- routes/login.js
+++ routes/login.js
@@ -27,7 +27,7 @@ route.post("/login", async (req, res) => {
     );
 
     res.cookie("token", token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
